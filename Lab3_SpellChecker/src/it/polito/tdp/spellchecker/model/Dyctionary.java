334bd0e8d4a9f6262da5cc12396225e3ffdd8e02
gianni arigliani <gianni arigliani@192.168.1.134>
2017-03-27 17:36:42 +0200

    ricerca dicotomica. (persiste problemino nel selezionare vocabolario)

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Dyctionary.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Dyctionary.java
@@ -8,10 +8,14 @@ import java.util.*;
 
 public class Dyctionary {
 	//private List<String> set= new LinkedList<String>();
-	private HashSet<String> set =new HashSet<String>();
+	//private HashSet<String> set =new HashSet<String>();
+	private ArrayList<String> set= new ArrayList<String>();
 	private List<RichWord> controllate= new LinkedList<RichWord>();
 	
-	public HashSet<String> getSet(){
+	//public HashSet<String> getSet(){
+		//return this.set;
+//	}
+	public ArrayList<String> getSet(){
 		return this.set;
 	}
 	
@@ -59,14 +63,66 @@ public class Dyctionary {
 	
 	public List<RichWord> spellCheckText(List<String> inputTextList){		
 		for(String c: inputTextList){
-			this.controllaParola(c);		
-			
+		//	this.controllaParola(c);		
+			this.ricercaDicotomica(c, 1, set.size()-1);
 		}	
 		
 		return controllate;
 		
 	}
+	private boolean ricercaDicotomica(String parola, int low, int high) {
+		parola.trim();
+		parola.replaceAll("[ \\p{Punct}]", "");
+		 int mid;
+       mid = (low + high)/2;
+        if ((mid < low)||(high<0))
+        {      RichWord temp= new RichWord(parola,false);
+	        	controllate.add(temp);
+               return false; //Valore non trovato
+        }
+        else if( parola.compareTo(set.get(mid))<0){
+        
+        	return ricercaDicotomica(parola, low, mid-1);
+        }
+        else if(parola.compareTo(set.get(mid))>0){
+        	return ricercaDicotomica(parola, mid+1, high);
+        }
+        else{
+        	RichWord temp= new RichWord(parola,true);
+        	controllate.add(temp);
+           return true; //Valore non trovato
+        	
+        }
+		
+		
+	}
+
+	/*
+	 * public int ricercaBinariaRicorsiva(int[] lista, int key, int low, int high)
+{
+        int mid;
+       mid = (low + high)/2;
+        if ((mid < low)||(high<0))
+        {
+                return -1; //Valore non trovato
+       }
+        else if (key < lista[mid])
+        {
+                return ricercaBinariaRicorsiva(lista, key, low, mid-1);
+        }
+        else if (key > lista[mid])
+       {
+                return ricercaBinariaRicorsiva(lista, key, mid+1, high);
+       }
+        else
+        {
+                return mid; //Valore trovato nella posizione mid
+        }
+}
+	 */
 	
+	
+
 	public boolean controllaParola(String parola){
 		parola.trim();
 		parola.replaceAll("[ \\p{Punct}]", "");
